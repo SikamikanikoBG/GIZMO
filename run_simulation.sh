11750ca7b8f7fd8e3fa5696328446139e293e37e
Arsen Apostolov <arsen.apostolov@bnpparibas-pf.bg>
2022-10-29 08:17:28 +0300

    3.14.5 - load data - if folder doesnt exists - create. fixing params and adding new ones

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,13 +1,15 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+# shellcheck disable=SC2164
 cd $DIR
-#echo $DIR/main.py ekont
 source activate gizmo
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurchf_sell&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurchf_buy&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurcad_sell&
-nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurcad_buy
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurcad_buy&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audnzd_sell&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audnzd_buy&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdusd_sell&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdusd_buy&
 
 
 
