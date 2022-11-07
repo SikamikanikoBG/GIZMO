81d2fb3799f98acf410cb40d80bf786a30a19bef
Arsen Apostolov <arsen.apostolov@bnpparibas-pf.bg>
2022-11-07 23:20:42 +0200

    update

--- run_gridsearch_winners.sh
+++ run_gridsearch_winners.sh
@@ -3,6 +3,8 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR
 source activate gizmo
 nice -n 16 python3 -W ignore $DIR/load_data.py --session gridsearch --volumes 15000
-nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdusd_buy --winner yes --tp 0.0025 --sl 0.006 --training_rows 10000 --period 480 --nb_features 30&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audnzd_buy --winner yes --tp 0.0025 --sl 0.004 --training_rows 10000 --period 480 --nb_features 50&
+
+
 
 
