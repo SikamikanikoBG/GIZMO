4b97e40dab0610039d5a1ac511baedfc7e6a9297
Arsen Apostolov <arsen.apostolov@bnpparibas-pf.bg>
2022-11-07 22:46:33 +0200

    param update

--- definitions.py
+++ definitions.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 args = None
-ngrok_prefix = "95fb"
+ngrok_prefix = "f032"
 api_url_prefix = "https://"+ngrok_prefix+"-95-43-20-41.eu.ngrok.io/"
 api_url_post_results_models = api_url_prefix + "bulk_upload_new_raw_data"
 api_url_post_results_predict = api_url_prefix + "upload_models_predict"
