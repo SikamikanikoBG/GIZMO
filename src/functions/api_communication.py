8f8372b449f7b1d1d2eea86db689a33c9d634a46
Arsen Apostolov <arsen.apostolov@bnpparibas-pf.bg>
2022-11-14 06:45:55 +0200

    3.19.4 - get data API notification small bug

--- src/functions/api_communication.py
+++ src/functions/api_communication.py
@@ -24,6 +24,7 @@ def api_get(url, conditions):
     response = requests.get(url)
     print(f"Response status: {response.status_code}")
     if response.status_code != 200:
-        notify_run.Notify.send(f"ArDi ERROR: API: Response status code: {response.status_code}")
+        notification = notify_run.Notify()
+        notification.send(f"ArDi ERROR: API: Response status code: {response.status_code}")
     df_received_data = pd.read_json(response.content)
     return df_received_data
