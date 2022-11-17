31cf405a6040b837614849200e4247b6bfa44be8
Arsen Apostolov <arsen.apostolov@bnpparibas-pf.bg>
2022-11-17 15:19:44 +0200

    requireme

--- run_predict.sh
+++ run_predict.sh
@@ -3,8 +3,9 @@ cd $DIR
 
 source activate gizmo
 python3 -W ignore $DIR/load_data.py --session predict --volumes 1000
-nice -n 10 python3 -W ignore $DIR/main.py --project ardi_eurcad_buy --predict_module standard --main_model xgb --pred_data_prep ardi --tp 0.0025 --sl 0.008 --period 480 --nb_tree_features 30&
-nice -n 10 python3 -W ignore $DIR/main.py --project ardi_nzdusd_buy --predict_module standard --main_model xgb --pred_data_prep ardi --tp 0.0025 --sl 0.01 --period 480 --nb_tree_features 30&
-nice -n 10 python3 -W ignore $DIR/main.py --project ardi_audnzd_buy_20221117 --predict_module standard --main_model xgb --pred_data_prep ardi  --tp 0.0025 --sl 0.008 --training_rows 4000 --period 240 --nb_features 50&
-nice -n 10 python3 -W ignore $DIR/main.py --project ardi_nzdusd_buy_20221117 --predict_module standard --main_model xgb --pred_data_prep ardi  --tp 0.0025 --sl 0.004 --training_rows 4000 --period 480 --nb_features 50&
-nice -n 10 python3 -W ignore $DIR/main.py --project ardi_eurcad_buy_20221117 --predict_module standard --main_model xgb --pred_data_prep ardi  --tp 0.0025 --sl 0.006 --training_rows 4000 --period 480 --nb_features 50&
+nice -n 10 python3 -W ignore $DIR/main.py --project ardi_eurcad_buy --predict_module standard --main_model xgb --pred_data_prep ardi --tp 0.0025 --sl 0.006 --period 480 --nb_tree_features 50&
+
+nice -n 10 python3 -W ignore $DIR/main.py --project ardi_audnzd_buy --predict_module standard --main_model xgb --pred_data_prep ardi --tp 0.0025 --sl 0.008 --period 240 --nb_tree_features 50&
+
+nice -n 10 python3 -W ignore $DIR/main.py --project ardi_nzdusd_buy --predict_module standard --main_model xgb --pred_data_prep ardi --tp 0.0025 --sl 0.004 --period 480 --nb_tree_features 50&
+
