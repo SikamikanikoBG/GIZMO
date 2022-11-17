4a986772508abf14022fce470d0a4bf659defd67
Arsen <arsen.apostolov@gmail.com>
2022-11-17 18:31:28 +0200

    grid

--- run_gridsearch_winners.sh
+++ run_gridsearch_winners.sh
@@ -1,8 +1,10 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
 cd $DIR
-source activate jizzmo2
-#nice -n 16 python3 -W ignore $DIR/load_data.py --session gridsearch --volumes 15000
+
+source /home/ardi/anaconda3/etc/profile.d/conda.sh
+conda activate jizzmo
+
+nice -n 16 python3 -W ignore $DIR/load_data.py --session gridsearch --volumes 15000
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audnzd_buy_20221117 --winner yes --tp 0.0025 --sl 0.008 --training_rows 4000 --period 240 --nb_features 50
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdusd_buy_20221117 --winner yes --tp 0.0025 --sl 0.004 --training_rows 4000 --period 480 --nb_features 50
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurcad_buy_20221117 --winner yes --tp 0.0025 --sl 0.006 --training_rows 4000 --period 480 --nb_features 50
--- run_simulation.sh
+++ run_simulation.sh
@@ -1,23 +1,24 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
 cd $DIR
-#conda init bash0
-source activate gizmo
+
+source /home/ardi/anaconda3/etc/profile.d/conda.sh
+conda activate jizzmo2
+
 nice -n 16 python3 -W ignore $DIR/load_data.py --session gridsearch --volumes 15000
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurcad_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurcad_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audnzd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audnzd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdusd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_xauusd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_xauusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpusd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-#nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_xauusd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_xauusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpusd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 
 
 
