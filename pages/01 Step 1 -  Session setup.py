cc53782ce14464cc68b6325f339d6db2dc708e3d
Arsen Apostolov <arsen.apostolov@bnpparibas-pf.bg>
2022-11-24 18:44:45 +0200

    4.00.0 - first GUI

--- pages/01 Step 1 -  Session setup.py
+++ pages/01 Step 1 -  Session setup.py
@@ -12,32 +12,35 @@ import definitions
 
 st.set_page_config(
     page_title="GIZMO - Setup",
-    page_icon="✅",
+    page_icon="random",
     layout="wide",
 )
 
-tab_sample_data, tab_settings, tab_run_data_prep  = st.tabs(["Sample data", "Settings", "Run data preparation"])
+tab_sample_data, tab_settings, tab_log = st.tabs(["Sample data", "Settings", "Log file"])
 
 with tab_sample_data:
-    data_file = st.file_uploader("Load 10000 rows sample data from the main table. Upload here the project dataset."
+    data_file = st.file_uploader("Load 2000 rows sample data from the main table. Upload here the project dataset."
                                  "Supported filetypes: csv, txt, parquet, feather, pickle.")
 
     if data_file:
         if "parquet" in str(data_file):
             input_df = pd.read_parquet(data_file)
-            if len(input_df) > 10000: input_df = input_df.sample(n=10000)
+
         elif "csv" in str(data_file):
             input_df = pd.read_csv(data_file)
-            if len(input_df) > 10000: input_df = input_df.sample(n=10000)
+
         elif "txt" in str(data_file):
             input_df = pd.read_csv(data_file)
-            if len(input_df) > 10000: input_df = input_df.sample(n=10000)
+
         elif "feather" in str(data_file):
             input_df = pd.read_feather(data_file)
-            if len(input_df) > 10000: input_df = input_df.sample(n=10000)
+
         else:
             input_df = pd.read_pickle(data_file)
-            if len(input_df) > 10000: input_df = input_df.sample(n=10000)
+
+        st.info(f"Data loaded with {len(input_df)} records.")
+        if len(input_df) > 20000:
+            input_df = input_df.sample(n=20000)
 
         target_feature = st.selectbox("Select which column is used for predictions: ", input_df.columns)
         if st.button("Use this column"):
@@ -45,14 +48,13 @@ with tab_sample_data:
         else:
             my_report = sv.analyze(input_df)
 
-        my_report.show_html(filepath= "./pages/EDA.html", open_browser=False, layout="vertical", scale=1.0)
+        my_report.show_html(filepath="./pages/EDA.html", open_browser=False, layout="vertical", scale=1.0)
 
         report_file = codecs.open("./pages/EDA.html", 'r')
         page = report_file.read()
-        components.html(page, width=1200, height=1000, scrolling=True)
+        components.html(page, width=1400, height=1000, scrolling=True)
 
 with tab_settings:
-
     # load projects
     params_path = f"{definitions.EXTERNAL_DIR}/params/"
 
@@ -60,7 +62,6 @@ with tab_settings:
 
     files_flags_dict = {}
     col1, col2, col3 = st.columns(3)
-
 
     project_list = []
     for proj in file_list:
@@ -72,7 +73,6 @@ with tab_settings:
     selected_project = st.sidebar.selectbox("Select existing project:", sorted(project_list))
     input_data_path = f"{definitions.ROOT_DIR}/input_data/{selected_project}"
     file_list_input_folder = os.listdir(input_data_path)
-
 
     st.header(selected_project)
     selected_param_file = str()
@@ -88,29 +88,36 @@ with tab_settings:
     try:
         with open(os.path.join(params_path + definitions.selected_param_file), 'r', encoding='utf-8') as param_file:
             json_object = json.load(param_file)
-            col1, col2, col_spacer ,col3, col4 = st.columns(5)
+            col1, col2, col_spacer, col3, col4 = st.columns(5)
 
             with st.form("Settings"):
 
                 with col1:
                     st.markdown("""#### Data processing settings""")
-                    new_value_criterion_column = st.text_input(label=f"{'criterion_column'}.", value=json_object['criterion_column'])
-                    new_value_custom_calculations = st.text_input(label=f"{'custom_calculations'}",value=json_object['custom_calculations'])
+                    new_value_criterion_column = st.text_input(label=f"{'criterion_column'}.",
+                                                               value=json_object['criterion_column'])
+                    new_value_custom_calculations = st.text_input(label=f"{'custom_calculations'}",
+                                                                  value=json_object['custom_calculations'])
 
-
-                    new_value_main_table = st.text_input(label=f"{'main_table'}. Well, the name of the main data file for the project.", value=json_object['main_table'])
-                    new_value_additional_tables = st.text_input(label=f"{'additional_tables'}. Jizzmo will left join them to the main table based on keys that you are specifying here.",
-                                                                value=json_object['additional_tables'])
+                    new_value_main_table = st.text_input(
+                        label=f"{'main_table'}. Well, the name of the main data file for the project.",
+                        value=json_object['main_table'])
+                    new_value_additional_tables = st.text_input(
+                        label=f"{'additional_tables'}. Jizzmo will left join them to the main table based on keys that you are specifying here.",
+                        value=json_object['additional_tables'])
                     new_value_observation_date_column = st.text_input(label=f"{'observation_date_column'}",
                                                                       value=json_object['observation_date_column'])
-                    new_value_periods_to_exclude = st.text_input(label=f"{'periods_to_exclude'}. Example - no full performance period, or bad nb of cases etc.",
-                                                                 value=json_object['periods_to_exclude'])
+                    new_value_periods_to_exclude = st.text_input(
+                        label=f"{'periods_to_exclude'}. Example - no full performance period, or bad nb of cases etc.",
+                        value=json_object['periods_to_exclude'])
                     new_value_under_sampling = st.text_input(label=f"{'under_sampling'}",
                                                              value=json_object['under_sampling'])
                     new_value_optimal_binning_columns = st.text_input(label=f"{'optimal_binning_columns'}",
                                                                       value=json_object['optimal_binning_columns'])
                     new_value_missing_treatment = st.text_input(label=f"{'missing_treatment'}",
                                                                 value=json_object['missing_treatment'])
+                    new_value_columns_to_include = st.text_input(label=f"{'columns_to_include'}",
+                                                                 value=json_object['columns_to_include'])
                 with col2:
                     st.markdown("""#### Based on sample data:""")
                     new_value_criterion_column = st.selectbox(f"{'criterion_column'}", input_df.columns.tolist())
@@ -165,23 +172,31 @@ with tab_settings:
                 json_object["cut_offs"] = new_value_cut_offs
                 json_object["under_sampling"] = new_value_under_sampling
                 json_object["optimal_binning_columns"] = new_value_optimal_binning_columns
+                json_object["columns_to_include"] = new_value_columns_to_include
 
                 new_project_name = st.text_input(label=f"Enter the name of the new project")
                 submitted_new = st.form_submit_button("Save as new project")
                 submitted = st.form_submit_button("Update current project")
                 if submitted:
-                    with open(os.path.join(params_path + selected_param_file), 'w', encoding='utf-8') as output_param_file:
+                    with open(os.path.join(params_path + selected_param_file), 'w',
+                              encoding='utf-8') as output_param_file:
                         json.dump(json_object, output_param_file)
                 if submitted_new:
-
                     new_param_file_name = f"params_{new_project_name}.json"
-                    with open(os.path.join(params_path + new_param_file_name), 'w', encoding='utf-8') as output_param_file:
+                    with open(os.path.join(params_path + new_param_file_name), 'w',
+                              encoding='utf-8') as output_param_file:
                         json.dump(json_object, output_param_file)
                 definitions.params = json_object
     except Exception as e:
         st.write(f"ERROR: Issue with {file}: {e}")
 
-with tab_run_data_prep:
+with tab_log:
     if st.button("Run GIZMO data preparation"):
-        subprocess.call(["python", "main.py", "--project", f"{selected_project}", "--data_prep_module", "standard"],
-                        stdout=open(f"{definitions.EXTERNAL_DIR}/logs/data_prep_{selected_project}.txt", "a"))
+        with st.spinner("Running data preparation"):
+            subprocess.call(
+                ["python", "main.py", "--project", f"{selected_project}", "--data_prep_module", "standard"],
+                stdout=open(f"{definitions.EXTERNAL_DIR}/logs/data_prep_{selected_project}.txt", "a"))
+        st.success('Done! Check the log!')
+
+        log_file = open(f"{definitions.EXTERNAL_DIR}/logs/data_prep_{selected_project}.txt", "r")
+        st.code(log_file.read())
--- pages/02 Step 2 - EDA after data processing.py
+++ pages/02 Step 2 - EDA after data processing.py
@@ -18,29 +18,60 @@ import definitions
 
 st.set_page_config(
     page_title="GIZMO - Exploratory Data Analysis after data processing",
-    page_icon="✅",
+    page_icon="random",
     layout="wide",
 )
 
-data_file = st.file_uploader("Load sample data from the main table.")
-final_features = st.file_uploader("Upload final features.")
-final_features = pd.read_pickle(final_features)
+# load projects
+params_path = f"{definitions.EXTERNAL_DIR}/params/"
+file_list = os.listdir(params_path)
+files_flags_dict = {}
+
+project_list = []
+for proj in file_list:
+    if '.json' in proj:
+        _, b = proj.split("params_")
+        a, _ = b.split(".json")
+        project_list.append(a)
+
+selected_project = st.sidebar.selectbox("Select existing project:", sorted(project_list))
+output_data_path = f"{definitions.ROOT_DIR}/output_data/{selected_project}"
+for file in file_list:
+    if selected_project in file:
+        with open(os.path.join(params_path + file), 'r', encoding='utf-8') as param_file:
+            json_object = json.load(param_file)
+            criterion_column = json_object["criterion_column"]
+
+with st.spinner("Loading output data"):
+    try:
+        input_df = pd.read_parquet(f"{output_data_path}/output_data_file_full.parquet")
+    except:
+        input_df = pd.read_parquet(f"{output_data_path}/output_data_file.parquet")
+st.success("Data loaded")
+
+final_features = pd.read_pickle(f"{output_data_path}/final_features.pkl")
 final_features = final_features[0:10]
+final_features.append(criterion_column)
 # final_features.append(definitions.params["criterion_column"])
 # st.write(final_features)
 
-if st.button("Load graphs"):
-    input_df = pd.read_parquet(data_file)
+if st.sidebar.button("Generate EDA"):
     input_df = input_df[final_features].copy()
-    pr = input_df.profile_report()
 
-    st_profile_report(pr)
+    with st.spinner("Generating graphs"):
+        # pr = input_df.profile_report()
+        # st_profile_report(pr)
+        # components.html(profile, width=1400, height=1000, scrolling=True)
 
-    #components.html(profile, width=17400, height=1000, scrolling=True)
+        my_report = sv.analyze(input_df, target_feat=criterion_column)
+        my_report.show_html(filepath=f"{output_data_path}/EDA.html", open_browser=False, layout="vertical", scale=1.0)
+    st.success("Graphs generated!")
 
-    #my_report = sv.analyze(input_df)
-    #my_report.show_html(filepath="./pages/EDA.html", open_browser=False, layout="vertical", scale=1.0)
+    report_file = codecs.open(f"{output_data_path}/EDA.html", 'r')
+    page = report_file.read()
+    components.html(page, width=1400, height=1000, scrolling=True)
 
-    #report_file = codecs.open("./pages/EDA.html", 'r')
-    #page = report_file.read()
-    #components.html(page, width=17400, height=1000, scrolling=True)
+if st.sidebar.button("Load existing EDA"):
+    report_file = codecs.open(f"{output_data_path}/EDA.html", 'r')
+    page = report_file.read()
+    components.html(page, width=1400, height=1000, scrolling=True)
