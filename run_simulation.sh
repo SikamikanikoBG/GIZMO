313db84677022fe88434bc503d1507773f434fa8
Arsen <arsen.apostolov@gmail.com>
2022-12-07 09:30:36 +0200

    new models

--- run_simulation.sh
+++ run_simulation.sh
@@ -22,7 +22,6 @@ nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurusd_buy --tp 
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-wait
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audcad_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audcad_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
@@ -34,12 +33,36 @@ nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_audusd_sell --tp
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadjpy_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_cadjpy_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_chfjpy_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+wait
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_chfjpy_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurgbp_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurgbp_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurnzd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurnzd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
-
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_euraud_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_euraud_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurjpy_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_eurjpy_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpaud_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpaud_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpcad_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpcad_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpjpy_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpjpy_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpnzd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_gbpnzd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdjpy_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_nzdjpy_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_usdcad_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_usdcad_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_usdchf_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_usdchf_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_usdjpy_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_usdjpy_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_xagusd_buy --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
+nice -n 16 python3 -W ignore $DIR/grid_search.py --project ardi_xagusd_sell --tp 0.0025 --sl 0.006 --training_rows 7000 --period 480 --nb_features 50&
 wait
 rm simulations.running
 
